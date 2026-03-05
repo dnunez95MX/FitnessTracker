ad1f3cbf751806313ea933e243e0ce0bc0af603a
dnunez <dnunez@ols-bio.de>
2026-03-05 12:30:59 +0100

    Initial commit

---
+++ FitnessTracker.API/FitnessTracker.Infrastructure/DependendyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
+
+namespace FitnessTracker.Infrastructure;
+
+public static class DependendyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services;
+    }
+}
